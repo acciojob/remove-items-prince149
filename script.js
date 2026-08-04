032f988fed8c030d6ce9c8140ddd8a4325bcee96
Prince Thakur <prince9373112766@gmail.com>
2026-08-04 13:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+const select = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click", function () {
+    select.remove(select.selectedIndex);
+});
